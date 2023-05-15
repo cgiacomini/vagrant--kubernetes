516e5c06552786ab62e991e3636007b250e47bcc
Corrado Giacomini <giacomini.corrado@free.fr>
2023-05-15 15:44:40 +0200

    Update example-001v1.py

--- examples/021-Observability/Example1v1/example-001v1.py
+++ examples/021-Observability/Example1v1/example-001v1.py
@@ -1,4 +1,5 @@
 import http.server
+import random
 from prometheus_client import start_http_server
 from prometheus_client import Counter
 
@@ -10,8 +11,9 @@ from prometheus_client import Counter
 # Metrics are automatically registered with the client library in the default registry.
 # A registry is a place where metrics are registered, to be exposed
 
-REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
-WRONG_REQUESTS = Counter('wrong_requests',     'Wrong Requests.')
+REQUESTS = Counter('hello_worlds_requests',    'Hello Worlds requested.')
+WRONG_REQUESTS = Counter('wrong_requests',     'Wrong Requests received.')
+EXCEPTIONS = Counter('hello_world_exceptions', 'Exceptions serving Hello World.')
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
@@ -22,6 +24,10 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b'Hello World')
+
+            with EXCEPTIONS.count_exceptions():
+               if random.random() < 0.2:
+                  raise Exception
         else:
             # Increment metric
             WRONG_REQUESTS.inc()
@@ -31,4 +37,6 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 if __name__ == "__main__":
     start_http_server(8000)
     server = http.server.HTTPServer(('', 8001), MyHandler)
+    print('Prometheus metrics available on port 8000 /metrics')
+    print('HTTP serveravailable on port 8001')
     server.serve_forever()
